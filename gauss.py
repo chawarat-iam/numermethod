71a2a45f050a0dbf731416f5cbe09ba1a0db400a
okilllar <chawarat.iam@gmail.com>
2019-03-27 19:12:59 +0700

    done

--- gauss.py
+++ gauss.py
@@ -1,5 +1,6 @@
 #Chawarat Iampramoon 5980028
 #Tommy Egnehall 5980011
+#python 3
 from decimal import *
 def truncate(n, decimals=0):
     multiplier = 10 ** decimals
@@ -18,21 +19,16 @@ def displayFunc(A):
 def gauss(A):
     n = len(A)
     for i in range(0, n):
-        # Search for max value in the column
         maxEl = abs(A[i][i])
         maxRow = i
         for k in range(i+1, n):
             if abs(A[k][i]) > maxEl:
                 maxEl = abs(A[k][i])
                 maxRow = k
-
-        # Swap max row with the  current row (one column at a time)
         for k in range(i, n+1):
             tmp = A[maxRow][k]
             A[maxRow][k] = A[i][k]
             A[i][k] = tmp
-
-        # Make all rows below the current column = 0
         for k in range(i+1, n):
             c = -A[k][i]/A[i][i]
             for j in range(i, n+1):
@@ -40,8 +36,6 @@ def gauss(A):
                     A[k][j] = 0
                 else:
                     A[k][j] += c * A[i][j]
-
-    # Solve for upper triangle
     x = [0 for i in range(n)]
     for i in range(n-1, -1, -1):
         x[i] = A[i][n]/A[i][i]
@@ -49,24 +43,22 @@ def gauss(A):
             A[k][n] -= A[k][i] * x[i]
     return x
 
-
 if __name__ == "__main__":
+    print("matrix size:")
     n = eval(input())
     A = [[0 for j in range(n+1)] for i in range(n)]
-
-    # Read the input
+    print("matrix values:")
     for i in range(0, n):
         line = list(map(Decimal, input().split(" ")))
         for j, el in enumerate(line):
             A[i][j] = el
-    input()
-
+    input("Press enter to skip this line ")
+    print("C values:")
     line = input().split(" ")
     lastLine = list(map(Decimal, line))
     for i in range(0, n):
         A[i][n] = lastLine[i]
 
-    # Display the input
     n = len(A)
     for i in range(0, n):
         line = ""
@@ -77,12 +69,9 @@ if __name__ == "__main__":
         print(line)
     print("")
 
-    # Calculation
-    #x = gauss(A)
     n = len(A)
 
     for i in range(0, n):
-        # Search for max value in the column
         maxEl = abs(A[i][i])
         maxRow = i
         for k in range(i+1, n):
@@ -90,13 +79,11 @@ if __name__ == "__main__":
                 maxEl = abs(A[k][i])
                 maxRow = k
 
-        # Swap max row with the  current row (one column at a time)
         for k in range(i, n+1):
             tmp = A[maxRow][k]
             A[maxRow][k] = A[i][k]
             A[i][k] = tmp
 
-        # Make all rows below the current column = 0
         for k in range(i+1, n):
             c = -A[k][i]/A[i][i]
             for j in range(i, n+1):
@@ -105,15 +92,13 @@ if __name__ == "__main__":
                 else:
                     A[k][j] += c * A[i][j]
 
-    # Solve for upper triangle
     x = [0 for i in range(n)]
     for i in range(n-1, -1, -1):
         x[i] = A[i][n]/A[i][i]
         for k in range(i-1, -1, -1):
             A[k][n] -= A[k][i] * x[i]
-    #return x
-    # Print final result
-    line = "Answer:\t"
+
+    line = "Answers:\t"
     for i in range(0, n):
         line += str(truncate(x[i],5)) + " \t"
     print (line)
--- interpolation.py
+++ interpolation.py
@@ -1,58 +1,44 @@
-def u_cal(u, n):
+#Chawarat Iampramoon 5980028
+#Tommy Egnehall 5980011
+#python 3
 
-    temp = u;
-    for i in range(1, n):
-        temp = temp * (u - i);
-    return temp;
-
-# calculating factorial of given number n
-def fact(n):
-    f = 1;
-    for i in range(2, n + 1):
-        f *= i;
-    return f;
-
-# Driver Code
 if __name__ == "__main__":
     n=int(input("Number of values given: "))
     print("X values: ")
-    x=[int(x) for x in input().split()]
-#n = 4;
-#x = [ 45, 50, 55, 60 ];
-
-# y[][] is used for difference table
-# with y[][0] used for input
-y = [[0 for i in range(n)]
-        for j in range(n)];
-    #print("F(x) values: ")
-    #i=[int(i) for i in input().split()];
-
-y[0][0] = 0.7071;
-y[1][0] = 0.7660;
-y[2][0] = 0.8192;
-y[3][0] = 0.8660;
-
-# Calculating the forward difference
+    x=[float(x) for x in input().split()]
+    y = [[0 for i in range(n)]
+            for j in range(n)];
+    print("F(x) values: ")
+    hold=input().split()
+    for i in range(len(y)):
+        for j in range(len(y)):
+            y[i][0]=float(hold[i])
 # table
 for i in range(1, n):
     for j in range(n - i):
         y[j][i] = y[j + 1][i - 1] - y[j][i - 1];
 
-# Displaying the forward difference table
+# Display
 for i in range(n):
     print(x[i], end = "\t");
     for j in range(n - i):
         print(y[i][j], end = "\t");
     print("");
+def u_cal(u, n):
+    temp = u;
+    for i in range(1, n):
+        temp = temp * (u - i);
+    return temp;
 
-# Value to interpolate at
-value = int(input("Value to interpolate at: "))
-
-# initializing u and sum
+def factorial(n):
+    f = 1;
+    for i in range(2, n + 1):
+        f *= i;
+    return f;
+interpolated = int(input("Value to interpolate at x = "))
 sum = y[0][0];
-u = (value - x[0]) / (x[1] - x[0]);
+u = (interpolated - x[0]) / (x[1] - x[0]);
 for i in range(1,n):
-    sum = sum + (u_cal(u, i) * y[0][i]) / fact(i);
+    sum = sum + (u_cal(u, i) * y[0][i]) / factorial(i);
 
-print("\nValue at", value,
-      "is", round(sum, 6));
+print("\nValue at x ", interpolated, "is", round(sum, 6));
